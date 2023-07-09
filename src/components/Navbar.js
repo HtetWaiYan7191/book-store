b2f12b72d39d5fa233d12ebfb0f65f7fa9f78d6e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-09 14:50:50 +0630

    Use NavLink in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BiSolidUser } from 'react-icons/bi';
+import { NavLink } from 'react-router-dom';
+import '../style.css';
 
 const Navbar = () => (
   <nav className=" flex justify-between py-10 px-16 items-center align-middle shadow-md">
     <ul className=" flex justify-around w-[40%]  items-center">
       <h2 className=" text-4xl text-sky-500 font-semibold">Book Store</h2>
-      <li>BOOKS</li>
-      <li>CATEGORIES</li>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/category">Categories</NavLink></li>
     </ul>
 
     <div>
