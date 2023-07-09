b8dd4daa7c8acb6d75325e8d5dc0c0424009381f
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-09 15:15:29 +0630

    Pass id props since it is necessary in delete process

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Books = ({ book, author }) => (
+const Books = ({
+  book, author, id, delBook,
+}) => (
   <div className="book-card flex justify-around border border-1 py-5 my-5">
     <h2>
       this is
@@ -11,13 +13,15 @@ const Books = ({ book, author }) => (
       {' '}
       {author}
     </h2>
-    <button type="button" className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Remove Book</button>
+    <button type="button" onClick={() => delBook(id)} className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Remove Book</button>
   </div>
 );
 
 Books.propTypes = {
   book: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  delBook: PropTypes.func.isRequired,
 };
 
 export default Books;
