ad068a7147e2c3e8612f693d82593d55cb87ca88
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-09 14:51:37 +0630

    Implement Category component

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Categories = () => (
+  <div>
+    <Navbar />
+    <h1>This is Category Page</h1>
+  </div>
+);
+
+export default Categories;
