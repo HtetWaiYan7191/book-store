b5eb609e8b5db9a88c59b97a1d6c281502915b9c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-09 14:50:18 +0630

    Change the output structure and add remove button

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Books = ({ book }) => (
-  <div className="book-card">
+const Books = ({ book, author }) => (
+  <div className="book-card flex justify-around border border-1 py-5 my-5">
     <h2>
       this is
       {book}
+      {' '}
+      by
+      {' '}
+      {author}
     </h2>
+    <button type="button" className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Remove Book</button>
   </div>
 );
 
 Books.propTypes = {
   book: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 
 export default Books;
