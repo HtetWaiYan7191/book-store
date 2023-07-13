25833950109b32fceced6099135c575356326ef0
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-13 19:53:02 +0630

    Replace delBooks function with dispatch deleteBooks

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteBooks } from '../features/books/booksSlice';
 
-const Books = ({
-  book, author, id, delBook,
-}) => (
-  <div className="book-card flex justify-around border border-1 py-5 my-5">
-    <h2>
-      This is
-      {' '}
-      {book}
-      {' '}
-      by
-      {' '}
-      {author}
-    </h2>
-    <button type="button" onClick={() => delBook(id)} className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Remove Book</button>
-  </div>
-);
+function Books({ book }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="book-card flex justify-between border border-1 py-5 my-5">
+      <h2>
+        This is
+        {' '}
+        {book.title}
+        {' '}
+        by
+        {' '}
+        {book.author}
+      </h2>
+      <button type="button" onClick={(e) => { e.preventDefault(); dispatch(deleteBooks(book.item_id)); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Remove Book
+      </button>
+    </div>
+  );
+}
 
 Books.propTypes = {
-  book: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  delBook: PropTypes.func.isRequired,
+  book: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default Books;
