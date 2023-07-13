6c62ac1d669316a0e75498415ad9cee69136798a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-13 22:29:44 +0630

    Get Api key from bookstore api

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Books from './Books';
 
 const BooksContainer = () => {
   const bookList = useSelector((state) => state.books.value);
+  const dispatch = useDispatch();
+
+  const fetchApi = async () => {
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+      const api = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/', settings);
+      const data = await api.text();
+      console.log(data)
+  }
+  
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
     <div className="book-container w-[65%] mx-auto my-10">
       {bookList.map((book) => (
