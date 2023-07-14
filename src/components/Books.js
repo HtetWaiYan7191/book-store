550eac2eb37e202dda96334cce735de0ac0ec238
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-14 21:24:05 +0630

    call dispatch in the onClick remove

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,11 +4,7 @@ import { useDispatch } from 'react-redux';
 import { removeBookFromApi } from '../features/books/booksSlice';
 
 function Books({ book }) {
-  console.log(book);
   const dispatch = useDispatch();
-  const removeBook = (book) => {
-    dispatch(removeBookFromApi(book));
-  };
   return (
     <div className="book-card flex justify-between border border-1 py-5 my-5">
       <h2>
@@ -20,7 +16,7 @@ function Books({ book }) {
         {' '}
         {book.author}
       </h2>
-      <button type="button" onClick={(e) => { e.preventDefault(); removeBook(book); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button type="button" onClick={(e) => { e.preventDefault(); dispatch(removeBookFromApi(book.item_id)); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Remove Book
       </button>
     </div>
