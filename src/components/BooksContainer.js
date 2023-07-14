5bd370bb15f5a4a067a30ed9ea6b70e30da2d3ec
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-14 16:09:59 +0630

    use dispatch and fetchBook function

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,30 +1,28 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Books from './Books';
+import { fetchBook } from '../features/books/booksSlice';
 
 const BooksContainer = () => {
   const bookList = useSelector((state) => state.books.value);
   const dispatch = useDispatch();
 
-  const fetchApi = async () => {
-    const settings = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-    const api = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/', settings);
-    const data = await api.text();
-    console.log(data);
-  };
-
   useEffect(() => {
-    fetchApi();
-  }, []);
+    dispatch(fetchBook());
+  }, [dispatch]);
+
+  // const fetchBooks = async () => {
+  //   const api = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/', { method: 'POST' });
+  //   const result = await api.text();
+  //   console.log(result);
+  // };
+  // useEffect(() => {
+  //   fetchBooks();
+  // }, []);
+
   return (
     <div className="book-container w-[65%] mx-auto my-10">
-      {bookList.map((book) => (
+      {bookList?.map((book) => (
         <Books key={book.item_id} book={book} />
       ))}
     </div>
