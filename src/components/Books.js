cc5d8c00c7499a8437a59d9a321849d5fb2d6bfd
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-14 16:09:28 +0630

    Add removeBook function in books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { deleteBooks } from '../features/books/booksSlice';
+import { removeBookFromApi } from '../features/books/booksSlice';
 
 function Books({ book }) {
+  console.log(book);
   const dispatch = useDispatch();
+  const removeBook = (book) => {
+    dispatch(removeBookFromApi(book));
+  };
   return (
     <div className="book-card flex justify-between border border-1 py-5 my-5">
       <h2>
@@ -16,7 +20,7 @@ function Books({ book }) {
         {' '}
         {book.author}
       </h2>
-      <button type="button" onClick={(e) => { e.preventDefault(); dispatch(deleteBooks(book.item_id)); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button type="button" onClick={(e) => { e.preventDefault(); removeBook(book); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Remove Book
       </button>
     </div>
