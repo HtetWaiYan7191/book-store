e94bbb25b10fcca2ca25eedea170ce3200a434ab
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-15 15:50:22 +0630

    Implement style for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import '../style.css';
 const Navbar = () => (
   <nav className=" flex justify-between py-10 px-16 items-center align-middle shadow-md">
     <ul className=" flex justify-around w-[40%]  items-center">
-      <h2 className=" text-4xl text-sky-500 font-semibold">Book Store</h2>
+      <h2 className=" text-[1.785rem] text-sky-500 font-semibold">Book Store</h2>
       <li><NavLink to="/">Home</NavLink></li>
       <li><NavLink to="/category">Categories</NavLink></li>
     </ul>
