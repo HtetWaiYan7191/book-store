fb54fc84021721820a7f15266b21205f6e92062f
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-15 16:24:38 +0630

    Finish first part of styling for book cards

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,23 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBookFromApi } from '../features/books/booksSlice';
+import '../style/Books.css';
 
 function Books({ book }) {
   const dispatch = useDispatch();
   return (
-    <div className="book-card flex justify-between border border-1 py-5 my-5">
-      <h2>
-        This is
-        {' '}
-        {book.title}
-        {' '}
-        by
-        {' '}
-        {book.author}
-      </h2>
-      <button type="button" onClick={(e) => { e.preventDefault(); dispatch(removeBookFromApi(book.item_id)); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Remove Book
-      </button>
+    <div className="book-card flex gap-5  border border-1 py-5 my-5">
+      <div className=" first-part w-[50%] pl-3">
+        <h2 className="book-category">{book.category}</h2>
+        <h2 className="book-title">{book.title}</h2>
+        <span className="book-text">{book.author}</span>
+        <ul className="user-features flex w-[40%] mt-4">
+          <li className="book-text me-3"><button type="button">Comments</button></li>
+          <div className="feature-verticle-line " />
+          <li className="book-text me-3">
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                dispatch(removeBookFromApi(book.item_id));
+              }}
+            >
+              Remove
+            </button>
+          </li>
+          <div className="feature-verticle-line" />
+          <li className="book-text me-3"><button type="button">Edit</button></li>
+        </ul>
+      </div>
+      <div className=" w-[25%] second-part bg-sky-500">
+        Second part
+      </div>
+      <div className=" w-[25%] third-part bg-red-500">
+        Third part
+      </div>
     </div>
   );
 }
