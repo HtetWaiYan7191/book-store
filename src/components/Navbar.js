4250f6c124a1320dc60dc190f38a4bd92d9035d4
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-16 23:20:04 +0630

    Add darkmode and font family for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { BiSolidUser } from 'react-icons/bi';
 import { NavLink } from 'react-router-dom';
+import { MdDarkMode } from 'react-icons/md';
+import PropTypes from 'prop-types';
+import { CiDark } from 'react-icons/ci';
 import '../style.css';
 
-const Navbar = () => (
-  <nav className=" flex justify-between py-10 px-16 items-center align-middle shadow-md">
+const Navbar = ({ handleDarkMode, isDarkMode }) => (
+  <nav className=" flex justify-between py-10 px-16 items-center align-middle shadow-md dark:bg-gray-800">
     <ul className=" flex justify-around w-[40%]  items-center">
-      <h2 className=" text-[1.785rem] text-sky-500 font-semibold">Book Store</h2>
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/category">Categories</NavLink></li>
+      <h2 className="  text-sky-500 font-semibold navbar-title">Book Store</h2>
+      <li className="navbar-text"><NavLink to="/" className="text-sky-500 dark:text-white-500">Home</NavLink></li>
+      <li className="navbar-text"><NavLink to="/category" className={isDarkMode ? 'text-white' : 'text-dark-500'}>Categories</NavLink></li>
     </ul>
 
-    <div>
-      <BiSolidUser className="text-2xl" />
+    <div className="flex align-middle">
+      <BiSolidUser className="text-2xl dark:text-sky-500 me-4" />
+      {isDarkMode ? (
+        <button type="button" onClick={handleDarkMode}>
+          <MdDarkMode className="text-2xl text-white" />
+        </button>
+      ) : (
+        <button type="button" onClick={handleDarkMode}>
+          <CiDark className="text-2xl " />
+        </button>
+      )}
     </div>
   </nav>
 );
+Navbar.defaultProps = {
+  handleDarkMode: () => {},
+};
+
+Navbar.propTypes = {
+  handleDarkMode: PropTypes.func,
+  isDarkMode: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default Navbar;
