c33618ead93e1dc371bf90e365d07227f64c2d51
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-16 23:20:37 +0630

    Add darkmode for Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,10 +10,10 @@ function Books({ book }) {
   const progress = Math.floor(Math.random() * 100);
   const chapter = Math.floor(Math.random() * 20);
   return (
-    <div className="book-card flex gap-5  border border-1 py-5 my-5 items-center align-bottom">
+    <div className="book-card flex gap-5  border border-1 py-5 my-5 items-center align-bottom dark:bg-gray-900">
       <div className=" first-part w-[40%] pl-5">
-        <h2 className="book-category">{book.category}</h2>
-        <h2 className="book-title">{book.title}</h2>
+        <h2 className="book-category dark:text-sky-700">{book.category}</h2>
+        <h2 className="book-title dark:text-gray-200">{book.title}</h2>
         <span className="book-text">{book.author}</span>
         <ul className="user-features flex w-[40%] mt-4">
           <li className="book-text me-3"><button type="button">Comments</button></li>
@@ -52,18 +52,18 @@ function Books({ book }) {
           }}
         />
         <div className="flex flex-col">
-          <h2 className="progress-percent">
+          <h2 className="progress-percent dark:text-sky-400">
             {progress}
             {' '}
             %
           </h2>
-          <span className="progress-text">Completed</span>
+          <span className="progress-text dark:text-gray-300">Completed</span>
         </div>
       </div>
       <div className="verticle-line" />
       <div className=" w-[25%] third-part">
-        <h2 className="current-chapter">CURRENT CHAPTER</h2>
-        <h2 className="chapter">
+        <h2 className="current-chapter dark:text-gray-300">CURRENT CHAPTER</h2>
+        <h2 className="chapter dark:text-sky-400">
           Chapter
           {chapter}
         </h2>
